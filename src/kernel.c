37a1b59d821eb9fd4206d18cb1c99bc20f560126
wouter <wouter.vanrooy@gmail.com>
2010-03-18 22:34:56 +0100

    playing around with writing to the screen

--- src/kernel.c
+++ src/kernel.c
@@ -1,13 +1,36 @@
+unsigned int position;
+void realwrite(unsigned int pos,unsigned int val)
+{
+	char *vidmem = (char *) 0xB8000;
+	vidmem[pos]=val+0x30;
+
+}
 void testwrite(char *writethis,unsigned int buff)
 {
 	char *vidmem = (char *) 0xB8000;
-	unsigned int i=0;
-	for (i=0;i < buff*2; i+=2)
+	unsigned int i = position;
+	for (i=i;i < (buff*2)+position; i+=2)
 	{
 		vidmem[i]=writethis[i/2];
+		vidmem[i+1]=0x07;
+		realwrite(20*(i+1),i);
 	}
+	position += (buff*2);
 }
 
+void clearscreen()
+{
+	char *vidmem = (char *) 0xB8000;
+	unsigned int i=0;
+	while(i<(80*50*2))
+	{
+		vidmem[i] = ' ';
+		i++;
+		vidmem[i] = 0x07;
+		i++;
+		position=0;
+	}
+}
 void kmain( void* mbd, unsigned int magic )
 {
    if ( magic != 0x2BADB002 )
@@ -15,7 +38,12 @@ void kmain( void* mbd, unsigned int magic )
       testwrite("Bootloader did not play nice",30);
    }
     char * boot_loader_name =(char*) ((long*)mbd)[16];
-    testwrite("testerder",9);
+    clearscreen();
+//    realwrite(150);
+    testwrite("ab",2);
+//    realwrite(170);
+    testwrite("cd",2);
+//    realwrite(180);
 }
 
 
