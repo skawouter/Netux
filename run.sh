da1ad011af0240ac42fc2ecdfc18771612be7e79
Wouter Van Rooy <wouter.vanrooy@gmail.com>
2013-09-03 20:43:03 +0200

    Add hd and enable-kvm to default run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 killall qemu
-qemu -fda bin/floppy.img -m 32 -net tap -net nic,model=rtl8139,macaddr=ab:cd:ef:21:43:65 &
+qemu -fda bin/floppy.img -m 32 -net tap -net nic,model=rtl8139,macaddr=ab:cd:ef:21:43:65 -enable-kvm -hda ./test.img &
